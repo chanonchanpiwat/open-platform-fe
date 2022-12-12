9715fedacb0f88db18d3a24b0896604102fca2ea
Chanon <chanon.chanpiwat@gmail.com>
2022-12-12 16:26:09 +0700

    add pre commit & remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<>Home page</>} />
-            <Route path="about" element={<>page 1</>} />
-        </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<>Home page</>} />
+                <Route path="about" element={<>page 1</>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
