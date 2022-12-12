f3e722f39abf8a52576f5adc60cc603a4e636f2e
Chanon <chanon.chanpiwat@gmail.com>
2022-12-12 19:43:53 +0700

    add react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 0,
+            suspense: true,
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </QueryClientProvider>,
 );
